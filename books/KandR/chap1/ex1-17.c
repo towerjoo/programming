4a514d4ae16eb24ab5879f474bd0b1e704d60bc2
Zhu Tao <zhutao.iscas@gmail.com>
2012-12-20 10:41:25 +0800

    finish 17 and 18

--- books/KandR/chap1/ex1-17.c
+++ books/KandR/chap1/ex1-17.c
@@ -6,9 +6,27 @@ author: Zhu Tao<zhutao.iscas@gmail.com>
 */
 #include<stdio.h>
 
+#define MAX_LINE_LEN 1000
+#define BORDER 80
 
 // Main Entry Point
 int main(){
-
+    int c;
+    char chars[MAX_LINE_LEN];
+    int i = 0;
+    while((c=getchar()) != EOF){
+        if (c == '\n'){
+            chars[i] = c;
+            chars[++i] = '\0';
+            if (i > BORDER) {
+                printf("%d: %s", i-1, chars);
+            }
+            i = 0; 
+        }
+        else {
+            // we don't check the max length here
+            chars[i++] = c;
+        }
+    }
     return 0;
 }
